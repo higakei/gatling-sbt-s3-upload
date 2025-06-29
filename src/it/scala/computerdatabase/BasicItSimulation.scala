973767b22d756f6922e95211e4d4da4469f053e2
higakei <higakei@hotmail.com>
2025-06-29 11:37:04 +0900

    warning除去

--- src/it/scala/computerdatabase/BasicItSimulation.scala
+++ src/it/scala/computerdatabase/BasicItSimulation.scala
@@ -1,19 +1,22 @@
 package computerdatabase
 
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
 import scala.concurrent.duration._
 
 class BasicItSimulation extends Simulation {
 
-  val httpProtocol = http
+  val httpProtocol: HttpProtocolBuilder = http
     .baseUrl("https://computer-database.gatling.io") // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
+  val scn: ScenarioBuilder = scenario("Scenario Name") // A scenario is a chain of requests and pauses
     .exec(http("request_1")
       .get("/"))
     .pause(7) // Note that Gatling has recorder real time pauses
